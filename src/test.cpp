4f66349345069846f35770411065973d64f45cff
Michael Ennen <mike.ennen@gmail.com>
2021-06-26 18:18:25 -0700

    Hash the alphabet!

--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,13 @@
 
 int main()
 {
-	std::cout << "Hello";
+	unsigned char hash[32] = {};
+	std::string data = "abcdefghijklmnopqrstuvwxyz";
+	CSHA256().Write((const unsigned char*) data.data(), data.size()).Finalize(hash);
+	std::cout << "Hash: " << std::endl;
+	for (int i = 0; i < 32; i++) {
+		std::cout << std::hex << (unsigned int) hash[i];
+	}
+	std::cout << std::endl;
 	return 0;
 }
